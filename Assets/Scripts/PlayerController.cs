cebd0474c31b5c7c5fd4792b2e44738bb39556ce
mo1s <seizedv2@ukr.net>
2022-12-30 19:19:49 +0200

    Animations
    Анимации персеножа. Начальная комната.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,49 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Vector2 direction;
     private Rigidbody2D rb;
+    private Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         direction.x = Input.GetAxisRaw("Horizontal");
         direction.y = Input.GetAxisRaw("Vertical");
+        if(direction.y == -1)
+        {
+            anim.SetBool("PlayerGuyWalkForward", true);
+        }
+        else
+        {
+            anim.SetBool("PlayerGuyWalkForward", false);
+        }
+        if(direction.y == 1)
+        {
+            anim.SetBool("PlayerGuyWalkBack", true);
+        }
+        else
+        {
+            anim.SetBool("PlayerGuyWalkBack", false);
+        }
+        if(direction.x == -1)
+        {
+            anim.SetBool("PlayerGuyWalkLeft", true);
+        }
+        else
+        {
+            anim.SetBool("PlayerGuyWalkLeft", false);
+        }
+        if(direction.x == 1)
+        {
+            anim.SetBool("PlayerGuyWalkRight", true);
+        }
+        else
+        {
+            anim.SetBool("PlayerGuyWalkRight", false);
+        }
     }
     void FixedUpdate()
     {
