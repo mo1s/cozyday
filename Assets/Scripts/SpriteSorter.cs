1447603d7347301c8fc20d8004997d2561208096
mo1s <seizedv2@ukr.net>
2022-12-30 13:19:54 +0200

    Spiter
    Добавил сортировку спрайтов.

--- Assets/Scripts/SpriteSorter.cs
+++ Assets/Scripts/SpriteSorter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpriteSorter : MonoBehaviour
 {
+    public bool isStatic = false;
     public float offset = 0;
     private int sortingOrderBase = 0;
     private Renderer renderer;
@@ -14,6 +15,9 @@ public class SpriteSorter : MonoBehaviour
     }
     private void LateUpdate()
     {
-        renderer.sortingOrder = (int)(sortingOrderBase - transform.position.y);
+        renderer.sortingOrder = (int)(sortingOrderBase - transform.position.y + offset);
+
+        if(isStatic)
+            Destroy(this);
     }
 }
